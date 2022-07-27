f516a17e3ef8fa0edf7c13833267636c5820e983
icetea0111 <sjlee7901@naver.com>
2022-07-27 15:39:53 +0900

    build movie app

--- src/App.js
+++ src/App.js
@@ -1,63 +1,53 @@
-import propTypes from "prop-types"
+import React from "react";
+import axios from "axios";
+import Movie from "./Movie";
+import "./static/App.css";
 
-River.propTypes = {
-  fav : propTypes.string.isRequired,
-  picture : propTypes.string.isRequired,
-  rating : propTypes.number.isRequired
-}
-
-const CoffeeList = [
-  {
-    id : 1,
-    name : "Latte",
-    image : "https://image.istarbucks.co.kr/upload/store/skuimg/2015/08/[110569]_20150813221315652.jpg",
-    rating : 5
-  },
-  {
-    id : 2,
-    name : "Americano",
-    image : "https://image.istarbucks.co.kr/upload/store/skuimg/2015/08/[110563]_20150813222100303.jpg",
-    rating : 4.2
-  },
-  {
-    id : 3,
-    name : "Cappuccino",
-    image : "https://image.istarbucks.co.kr/upload/store/skuimg/2015/08/[110601]_20150803101742023.jpg",
-    rating : 3
-  },
-  {
-    id : 4,
-    name : "Caffe Mocha",
-    image : "https://image.istarbucks.co.kr/upload/store/skuimg/2015/08/[46]_20150803105115048.jpg",
-    rating : 1
-  },
-  {
-    id : 5,
-    name : "Espresso",
-    image : "https://image.istarbucks.co.kr/upload/store/skuimg/2015/08/[20]_20150813221922951.jpg",
-    rating : 1
+class App extends React.Component {
+  state = {
+    isLoading: true,
+    movies: [],
+  };
+  getMovies = async () => {
+    const {
+      data: {
+        data: { movies },
+      },
+    } = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=like_count");
+    this.setState({ movies, isLoading: false });
+  };
+  componentDidMount() {
+    this.getMovies();
   }
-]
-
-function River({fav, picture,rating}){
-  return (
-    <div>
-      <h2>{fav}는 맛있다!</h2>
-      <img src={picture} alt={fav} />
-      <span>{rating}</span>
-    </div>
-  );
+  render() {
+    const { isLoading, movies } = this.state;
+    return (
+      <section className="container">
+        {isLoading ? (
+          <div className="loader">
+            <span className="loader_text">로딩중...</span>
+          </div>
+        ) : (
+          <div className="movies_container">
+            {movies.map((movie) => {
+              return (
+                <Movie
+                  key={movie.id}
+                  id={movie.id}
+                  year={movie.year}
+                  title={movie.title}
+                  summary={movie.summary}
+                  poster={movie.medium_cover_image}
+                  genres={movie.genres}
+                />
+              );
+            })}
+            ;
+          </div>
+        )}
+      </section>
+    );
+  }
 }
-
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello</h1>
-      {CoffeeList.map(menu => <River key={menu.id} fav={menu.name} picture={menu.image} rating={menu.rating} />)}
-    </div>
-  );
-}
-
-
 
 export default App;
